9ab14988eee5e1e788e4fbf1c18a24e9f3ebf0b9
Andrew <66430340+aandrew-me@users.noreply.github.com>
2023-04-30 20:21:45 +0600

    first commit

---
+++ db.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func connectDB() {
+	// MongoDB start
+	var err error
+	client, err = mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+
+	err = client.Connect(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Ping(ctx, nil)
+
+	if err != nil {
+		log.Fatal("Failed to ping MongoDB:", err)
+	} else {
+		fmt.Println("Successfully pinged MongoDB")
+	}
+}
---
+++ main.go
@@ -0,0 +1,288 @@
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"time"
+
+	"github.com/go-playground/validator"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/session"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type UserRegister struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=6,max=32"`
+}
+
+type User struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=6,max=32"`
+	UserID   string `json:"userID" validate:"required" bson:"userID"`
+	Notes    []Note `json:"notes"`
+}
+
+type Note struct {
+	ID    string `json:"id" required`
+	Title string `json:"title" required`
+	Body  string `json:"body" required`
+}
+
+var client *mongo.Client
+
+func main() {
+	connectDB()
+	defer client.Disconnect(context.Background())
+	app := fiber.New()
+
+	api := app.Group("/api")
+
+	api.Post("/register", register)
+	api.Post("/login", login)
+
+	api.Post("/info", AuthMiddleWare, info)
+
+	app.Listen(":3000")
+
+}
+
+// Auth
+var validate = validator.New()
+var store = session.New(session.Config{
+	CookieHTTPOnly: true,
+	Expiration:     time.Hour * 24 * 7,
+})
+
+// Register
+func register(c *fiber.Ctx) error {
+	var db = client.Database("enotesdb")
+	var userCollection = db.Collection("users")
+	c.Accepts("application/json")
+	var user UserRegister
+
+	err := c.BodyParser(&user)
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "something went wrong: " + err.Error(),
+			"status":  "false",
+		})
+	}
+
+	err = validate.Struct(&user)
+
+	if err != nil {
+		fmt.Println(err)
+		return c.Status(fiber.StatusNotAcceptable).JSON(fiber.Map{
+			"status":  "false",
+			"message": "Make sure password and email are following correct rules",
+		})
+	}
+
+	// Checking if email already exists
+
+	result := userCollection.FindOne(c.Context(), fiber.Map{"email": user.Email})
+	var userResult UserRegister
+	result.Decode(&userResult)
+	if result.Err() == nil {
+		return c.Status(fiber.StatusNotAcceptable).JSON(fiber.Map{
+			"status":  "false",
+			"message": "Email already in use",
+		})
+
+	}
+	randomString, err := GenerateRandomString(20)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "something went wrong: " + err.Error(),
+			"status":  "false",
+		})
+	}
+	finalUser := User{
+		Email:    user.Email,
+		Password: user.Password,
+		Notes:    []Note{},
+		UserID:   randomString,
+	}
+
+	_, err = userCollection.InsertOne(context.Background(), finalUser)
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "something went wrong: " + err.Error(),
+			"status":  "false",
+		})
+	}
+	sess, err := store.Get(c)
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "something went wrong: " + err.Error(),
+			"status":  "false",
+		})
+	}
+
+	sess.Set("userID", finalUser.UserID)
+
+	if err := sess.Save(); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Failed to set session cookie" + err.Error(),
+			"status":  "false",
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"status":  "true",
+		"message": "Account Created",
+	})
+
+}
+
+// func NewMiddleware() fiber.Handler {
+// 	return AuthMiddleWare
+// }
+
+func AuthMiddleWare(c *fiber.Ctx) error {
+	sess, err := store.Get(c)
+
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "not authorized",
+			"status":  "false",
+		})
+	}
+
+	userID := sess.Get("userID")
+
+	if userID == nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "not authorized",
+			"status":  "false",
+		})
+	}
+
+	fmt.Println(userID)
+	c.Locals("userID", userID)
+	return c.Next()
+}
+
+func info(c *fiber.Ctx) error {
+	userID := c.Locals("userID")
+
+	if userID == nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "not authorized",
+			"status":  "false",
+		})
+	}
+
+	var db = client.Database("enotesdb")
+	var userCollection = db.Collection("users")
+
+	result := userCollection.FindOne(context.Background(), fiber.Map{
+		"userID": userID,
+	})
+
+	if result.Err() != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "something went wrong: ",
+			"status":  "false",
+		})
+	}
+
+	var user UserRegister
+	result.Decode(&user)
+
+	return c.Status(200).JSON(user)
+}
+
+func login(c *fiber.Ctx) error {
+	var db = client.Database("enotesdb")
+	var userCollection = db.Collection("users")
+	c.Accepts("application/json")
+
+	sess, err := store.Get(c)
+	userID := sess.Get("userID")
+
+	if userID != nil {
+		return c.Status(fiber.StatusAccepted).JSON(fiber.Map{
+			"message": "Already logged in",
+			"status":  "true",
+		})
+	}
+
+	var user UserRegister
+
+	err = c.BodyParser(&user)
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "something went wrong: " + err.Error(),
+			"status":  "false",
+		})
+	}
+
+	err = validate.Struct(&user)
+
+	if err != nil {
+		fmt.Println(err)
+		return c.Status(fiber.StatusNotAcceptable).JSON(fiber.Map{
+			"status":  "false",
+			"message": "Make sure password and email are following correct rules",
+		})
+	}
+
+	result := userCollection.FindOne(context.Background(), fiber.Map{
+		"email": user.Email,
+	})
+
+	if result.Err() != nil {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"status":  "false",
+			"message": "The account does not exist",
+		})
+	}
+	var userResult User
+	result.Decode(&userResult)
+
+	if userResult.Password != user.Password {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"status":  "false",
+			"message": "Incorrect password",
+		})
+	}
+
+	sess, error := store.Get(c)
+
+	if error != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "something went wrong: " + err.Error(),
+			"status":  "false",
+		})
+	}
+	sess.Set("userID", userResult.UserID)
+	sess.Save()
+	c.Locals("userID", userResult.UserID)
+	return c.Status(fiber.StatusAccepted).JSON(fiber.Map{
+		"message": "Successfully logged in. Redirect to home page",
+		"status":  "true",
+	})
+
+}
+
+func GenerateRandomString(n int) (string, error) {
+	const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-"
+	ret := make([]byte, n)
+	for i := 0; i < n; i++ {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
+		if err != nil {
+			return "", err
+		}
+		ret[i] = letters[num.Int64()]
+	}
+
+	return string(ret), nil
+}
